fe7819a9397df1fbee6656a285e8a1f69d917742
Eric Ramming <eramming14@gmail.com>
2026-02-20 17:24:18 -0500

    Randomly creating input NNs based on string schematic of its arch

--- FC_NN.py
+++ FC_NN.py
@@ -2,7 +2,6 @@ from typing import Callable, List
 import numpy as np
 # from functools import reduce
 # import operator
-from FC_NN import FC_NN
 from Perceptron import Perceptron
 from Perceptron import InputNode
 
@@ -17,8 +16,8 @@ class FC_NN:
 
     def __init__(self, layers: List[Layer], inputs: List[InputNode],
                  eta: float = 1.0) -> None:
-        if len(layers) > 2:
-            raise ValueError("The current implementation can only handle NNs with up to 2 layers.")
+        # if len(layers) > 2:
+        #     raise ValueError("The current implementation can only handle NNs with up to 2 layers.")
         self.inputs: List[InputNode] = inputs
         self.p_layers: List[Layer] = layers
         # self.weight_cnt: int = reduce(operator.add, [len(p.w_vec) for layer in self.p_layers for p in layer.perceptrons], 0)
@@ -32,7 +31,7 @@ class FC_NN:
                     f_act: Callable[[float], float]) -> 'FC_NN':
         layers: List[Layer] = FC_NN.create_perceptron_layers(config, f_act)
         inputs: List[InputNode] = [InputNode() for _ in range(input_size)]
-        FC_NN.add_perceptron_inputs(layers, inputs)
+        FC_NN.add_bidirectional_connections(layers, inputs)
         return FC_NN(layers, inputs)
     
     @staticmethod
@@ -43,16 +42,18 @@ class FC_NN:
             perceptrons: List[Perceptron] = []
             for j, p_weights in enumerate(layer):
                 perceptrons.append(Perceptron(np.array(p_weights[:-1]),
-                                              p_weights[-1], f_act, [], f"P_{i}{j}"))
+                                              p_weights[-1], f_act, (i, j)))
             layers.append(Layer(perceptrons))
         return layers
     
     @staticmethod
-    def add_perceptron_inputs(layers: List[Layer], inputs: List[InputNode]) -> None:
-        layer_to_update: List[Perceptron] = layers[-1]
-        for layer in reversed(layers[1:]):
-            for p in layer_to_update:
-                p.inputs = layer.perceptrons
-            layer_to_update = layer
+    def add_bidirectional_connections(layers: List[Layer], inputs: List[InputNode]) -> None:
+        consuming_layer: List[Perceptron] = layers[-1].perceptrons
+        for producing_layer in [layer.perceptrons for layer in reversed(layers[:-1])]:
+            for p in consuming_layer:
+                p.set_producers(producing_layer)
+            for p in producing_layer:
+                p.set_consumers(consuming_layer)
+            consuming_layer = producing_layer
         for p in layers[0].perceptrons:
-            p.inputs = inputs
+            p.set_producers(inputs)
--- eramming-module5-NN.py
+++ eramming-module5-NN.py
@@ -2,21 +2,52 @@ from FC_NN import FC_NN
 from Activation import Activation
 from Trainer import Trainer
 from typing import List
+import numpy as np
+import random
+
+random.seed(777)
+
+
+class TestCase:
+
+    def __init__(self, arch_str: str) -> None:
+        self.arch: List[int] = list(map(int, arch_str.split("-")))
+        self.inputs: List[float] = self._create_inputs(self.arch[0])
+        self.targets: np.ndarray = self._create_targets(self.arch[-1])
+        self.nn: FC_NN = self._create_nn()
+
+    def _create_nn(self) -> FC_NN:
+        config = []
+        for i in range(1, len(self.arch)):
+            layer = [[random.random() for _ in range(self.arch[i-1] + 1)] for _ in range(self.arch[i])]
+            config.append(layer)
+        print(f"Config: {config}")
+        return FC_NN.from_config(self.arch[0], config, Activation.sigmoid)
+
+    def _create_inputs(self, in_size: int) -> List[float]:
+        sample = [1.9, 7, -2.2, -8.4, 3.9]
+        if in_size > len(sample):
+            raise ValueError(f"Current input max is {len(sample)}")
+        return sample[:in_size]
+
+    def _create_targets(self, terminal_cnt: int) -> np.ndarray:
+        sample = np.array([0.14, 0.33, 0.79, 0.5, 0.01])
+        if terminal_cnt > len(sample):
+            raise ValueError(f"Current terminal count max is {len(sample)}")
+        return sample[:terminal_cnt]
+
 
 
 def main() -> None:
-    init_weights = [
-        [[0.7, 0.7, 0.7, 0], [0.4, 0.4, 0.4, 0]],
-        [[0.5, 0.1, 0], [0.5, 0.1, 0], [0.5, 0.1, 0]],
-        [[0.3, 0.5, 0.7, 0], [0.4, 0.6, 0.8, 0]]
-    ]
-    nn: FC_NN = FC_NN.from_config(3, init_weights, Activation.sigmoid)
-    trainer = Trainer(5, 1.0, nn)
-    inputs = [1.9, 7, -2.2]
-    targets = [0.14]
-    trainer.train(inputs, targets)
-    prediction: List[float] = nn.predict()
-    print(f"Desired Value: {targets}\tPredicted Value(s): {prediction}")
+    tc = TestCase("2-1")
+    epochs: int = 50
+    eta: float = 1.0
+    trainer = Trainer(tc.nn, epochs, eta)
+
+    print(f"Inputs: {tc.inputs}\tTargets: {tc.targets}")
+    trainer.train(tc.inputs, tc.targets)
+    prediction: List[float] = tc.nn.predict()
+    print(f"Desired Value: {tc.targets}\tPredicted Value(s): {prediction}")
 
 
 if __name__ == "__main__":
